e0e8ca7e5e6bc2478cbf722274d12cc76309f209
Hariprasath <49398090+hariprasath-v@users.noreply.github.com>
2023-08-20 14:42:59 +0530

    Update umpire.py

--- umpire.py
+++ umpire.py
@@ -11,7 +11,7 @@ class Umpire:
         :param batter_stats: batter information
         :param bowler_stats: bowler information
         """
-        if batter_stats>bowler_stats: #if Batter stats greater than bowler stats then the possibility of out come changes
-            return random.choices([0,1],[0.2,0.8])[0] #60 percentage chance for legal delivery and 40 percentage chance for out(bowled)
-        else: #if Batter stats not greater than bowler stats then the possibility of out come changes
+        if batter_stats>bowler_stats: #if Batter stats are greater than bowler stats then the possibility of outcome changes
+            return random.choices([0,1],[0.2,0.8])[0] #80 percentage chance for legal delivery and 20 percentage chance for out(bowled)
+        else: #if Batter stats are not greater than bowler stats then the possibility of outcome changes
             return random.choices([0,1],[0.6,0.4])[0] #60 percentage chance for out(bowled) and 40 percentage chance for legal delivery
